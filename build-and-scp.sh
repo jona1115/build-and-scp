27b3088480c3163ce08d341baadc47e7a0b016fc
Jonathan Tan <114776594+jona1115@users.noreply.github.com>
2024-06-24 17:53:18 -0400

    Fix typo

--- build-and-scp.sh
+++ build-and-scp.sh
@@ -3,7 +3,7 @@
 # Check if the correct number of arguments are provided
 if [ "$#" -ne 5 ]; then
     echo "Usage: $0 <compile_path> <env_init_script> <compile_command> <exe_path> <target_path>"
-    printf "\nExample: ./build-and-scp.sh /home/jonathan/src/io2000 init-build-cbc9000-ti \"bitbake -c compile -f platformd\" /path/to/build/src/infos/platform-infos root@10.106.171.240:~"
+    printf "\nExample: ./build-and-scp.sh /home/jonathan/src init-environment.sh \"bitbake -c compile -f abc\" /home/jonathan/build/src/abc.exe root@192.168.1.1:~"
     printf "\n"
     exit 1
 fi
@@ -39,4 +39,4 @@ fi
 echo "Successfully compiled and copied the executable to the target path."
 
 # Copy-and-paste-able example:
-# ./build-and-scp.sh /home/jonathan/src/io2000 init-build-cbc9000-ti "bitbake -c compile -f platformd" /home/jonathan/src/io2000/build/cbc9000-ti/tmp/work/aarch64-eaton-linux/platformd/1.0+gitAUTOINC+4b42382605-r1/build/src/infos/platform-infos root@10.106.171.240:~
+# ./build-and-scp.sh /home/jonathan/src "bitbake -c compile -f abc" /home/jonathan/build/src/abc.exe root@192.168.1.1:~
